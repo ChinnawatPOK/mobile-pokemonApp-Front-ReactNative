83cc8d7147d1ed5a430abcdf17d179e2a6bdd5c8
ChinnawatPOK <60538900+ChinnawatPOK@users.noreply.github.com>
2021-03-06 16:05:50 +0700

    add initial screen

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
 
 const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator></Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
